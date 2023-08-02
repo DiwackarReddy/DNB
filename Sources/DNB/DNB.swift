4039fece630c1b81caa8aa217cc7382fcd6fff72
DiwackarReddy <diwackarreddy@gmail.com>
2023-08-02 12:03:38 +0530

    init

--- Sources/DNB/DNB.swift
+++ Sources/DNB/DNB.swift
@@ -2,6 +2,9 @@ public struct testFunction {
     public static func doSomeWork(){
         print("Doing some work")
     }
+    public init() {
+        
+    }
     public static func sum<T: Numeric>(_ a: T, _ b: T) -> T {
         print(a,b)
         return a + b
